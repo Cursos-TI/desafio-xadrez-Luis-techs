8027e546a38de0e0caae08d75f5dab2f6e92fc49
Luis-techs <luisgustavoac3@gmail.com>
2025-03-04 21:16:53 +0000

    Jogo xadrez

--- xadrez.c
+++ xadrez.c
@@ -1,32 +1,88 @@
 #include <stdio.h>
 
-// Desafio de Xadrez - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
-// O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
+// Codigo feito para o desafio do Xadrez, onde faremos o movimento das seguintes peças: Torre, Bispo, Rainha e Cavalo; - 04/03/2025
+// Implementamos um menu interativo para ficar mais divertido para o usuário; - 04/03/2025
+// Utilizamos a estrutura Do-While para fazer o menu interavivo, dando as o chance do jogador escolher a peça ou se deseja sair do jogo; - 04/03/2025
+// Utilizamos a estrutura de repetição While para simular a quantidade de casas que foi andada de acordo com a escolha do jogador; - 04/03/2025
 
-int main() {
-    // Nível Novato - Movimentação das Peças
-    // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
 
-    // Implementação de Movimentação do Bispo
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
 
-    // Implementação de Movimentação da Torre
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
 
-    // Implementação de Movimentação da Rainha
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
+int main(){
 
-    // Nível Aventureiro - Movimentação do Cavalo
-    // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
-    // Um loop pode representar a movimentação horizontal e outro vertical.
+    int opcao;
+    int casas;
+    int j;
+    int num = 1;
 
-    // Nível Mestre - Funções Recursivas e Loops Aninhados
-    // Sugestão: Substitua as movimentações das peças por funções recursivas.
-    // Exemplo: Crie uma função recursiva para o movimento do Bispo.
+    do{
 
-    // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
-    // Inclua o uso de continue e break dentro dos loops.
+        printf("*** MOVIMENTO DO XADREZ ***\n");
+        printf("[1] - Mover a Torre...\n");
+        printf("[2] - Mover o Bispo...\n");
+        printf("[3] - Mover a Rainha...\n");
+        printf("[4] - Mover o Cavalo...\n");
+        printf("[5] - Sair do jogo...\n");
+        printf("Digite uma das opções acima: ");
+        scanf("%d", &opcao);
+        printf("\n");
 
-    return 0;
+        switch(opcao){
+            case 1:
+
+                printf("Digite a quantidade de casas que deseja andar: ");
+                scanf("%d", &casas);
+
+                while(casas > 0){
+                    printf("Frente...\n");
+                    casas--;
+                }
+
+                printf("\n");
+                break;
+                
+
+            case 2: 
+                printf("Digite a quantidade de casas que deseja andar: ");
+                scanf("%d", &casas);
+
+                while(casas > 0){
+                    printf("Cima, Direita...\n");
+                    casas--;
+                }
+
+                printf("\n");
+                break;
+                
+
+            case 3:
+                printf("Digite a quantidade de casas que deseja andar: ");
+                scanf("%d", &casas);
+                    
+                while(casas > 0){
+                    printf("Esquerda...\n");
+                    casas--;
+                }
+
+                printf("\n");
+                break;
+
+            case 4: 
+                while(num--){
+                    for (j = 0; j < 2; j++){
+                        printf("Cima...\n");
+                    }
+                printf("Direita...\n");
+            }
+
+            printf("\n");
+            break;
+        
+        }
+
+    }while(opcao != 5);
+
+    printf("Saindo do jogo...");
+
 }
+
