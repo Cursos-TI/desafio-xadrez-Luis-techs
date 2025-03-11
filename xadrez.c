42d2f30606fa166933b13a19f24fc43192278a89
Luis-techs <luisgustavoac3@gmail.com>
2025-03-10 22:09:12 -0300

    atualização xadrez.c

--- xadrez.c
+++ xadrez.c
@@ -1,89 +1,68 @@
 #include <stdio.h>
 
-// Codigo feito para o desafio do Xadrez, onde faremos o movimento das seguintes peças: Torre, Bispo, Rainha e Cavalo; - 04/03/2025
-// Implementamos um menu interativo para ficar mais divertido para o usuário; - 04/03/2025
-// Utilizamos a estrutura Do-While para fazer o menu interavivo, dando as o chance do jogador escolher a peça ou se deseja sair do jogo; - 04/03/2025
-// Utilizamos a estrutura de repetição While para simular a quantidade de casas que foi andada de acordo com a escolha do jogador; - 04/03/2025
-// Tentativa de fazer um jogo de xadrez de forma interativa; - 04/03/2025
+// Criando procedimento para a torre
 
+void torre(int casas){
+    if (casas > 0){
+        printf("Direita...\n");
+        torre(casas - 1);
+    }
+}
 
+// Criando procedimento para o bispo
 
+void bispo(int casas2){
+    if (casas2 > 0){
+        printf("Cima, Direita...\n");
+        bispo(casas2 - 1);
+    }
+}
+
+// Criando procedimento para rainha
+
+void rainha(int casas3){
+    if(casas3 > 0){
+        printf("Esquerda...\n");
+        rainha(casas3 - 1);
+    }
+}
+
+// Criando procedimento para o cavalo
+
+void cavalo(int casas4){
+    int cavalo2 = 0;
+    if (casas4 > 0){
+        for(int cavalo = 0; cavalo < 1; cavalo++){
+            while (cavalo2 < 2)
+            {
+                printf("Cima..\n");
+                cavalo2++;
+            }
+            printf("Direita...");
+            
+        }
+    }
+}
 
 int main(){
 
-    int opcao;
-    int casas;
-    int j;
-    int num = 1;
+        int numero = 5;
+        torre(numero);  // Chamando o procedimento da torre
 
-    do{
-
-        printf("*** MOVIMENTO DO XADREZ ***\n");
-        printf("[1] - Mover a Torre...\n");
-        printf("[2] - Mover o Bispo...\n");
-        printf("[3] - Mover a Rainha...\n");
-        printf("[4] - Mover o Cavalo...\n");
-        printf("[5] - Sair do jogo...\n");
-        printf("Digite uma das opções acima: ");
-        scanf("%d", &opcao);
-        printf("\n");
-
-        switch(opcao){
-            case 1:
-
-                printf("Digite a quantidade de casas que deseja andar: ");
-                scanf("%d", &casas);
-
-                while(casas > 0){
-                    printf("Frente...\n");
-                    casas--;
-                }
-
-                printf("\n");
-                break;
-                
-
-            case 2: 
-                printf("Digite a quantidade de casas que deseja andar: ");
-                scanf("%d", &casas);
-
-                while(casas > 0){
-                    printf("Cima, Direita...\n");
-                    casas--;
-                }
-
-                printf("\n");
-                break;
-                
-
-            case 3:
-                printf("Digite a quantidade de casas que deseja andar: ");
-                scanf("%d", &casas);
-                    
-                while(casas > 0){
-                    printf("Esquerda...\n");
-                    casas--;
-                }
-
-                printf("\n");
-                break;
-
-            case 4: 
-                while(num--){
-                    for (j = 0; j < 2; j++){
-                        printf("Cima...\n");
-                    }
-                printf("Direita...\n");
-            }
-
-            printf("\n");
-            break;
+        printf("\n"); // Espaçamento entre a impressão da torre e do bispo
         
-        }
+        int numero2 = 5;
+        bispo(numero2);  // Chamando o procedimento do bispo
 
-    }while(opcao != 5);
+        printf("\n"); // Espaçamento entre a impressão do bispo e da rainha
 
-    printf("Saindo do jogo...");
+        int numero3 = 8;
+        rainha(numero3); // Chamando o procedimento da rainha
+
+        printf("\n"); // Espaçamento entre a impressão do rainha e do cavalo
+
+        int numero4 = 3;
+        cavalo(numero4); // Chamando o procedimento do cavalo
 
 }
 
